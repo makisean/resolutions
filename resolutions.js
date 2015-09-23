330f30c652255191d7a3788c3a7913d34afef3d0
makisean <makisean@gmail.com>
2015-09-23 17:45:17 -0500

    update 9.23

--- resolutions.js
+++ resolutions.js
@@ -1,13 +1,36 @@
+Resolutions = new Mongo.Collection('resolutions');
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    resolutions: [
-    { title: "Hello Resolution #1" },
-    { title: "Bye Resolution #1" },
-    { title: "Kanye West"}
-    ]
+    resolutions: function() {
+      return Resolutions.find();
+    }   
   });
 
+  Template.body.events({
+    "submit .new-resolution": function(event){
+     var title = event.target.title.value;
 
+      Resolutions.insert({
+        title: title,
+        createdAt: new Date()
+      });
+
+      event.target.title.value = "";
+ 
+      return false;
+      
+    }
+ });
+
+
+  Template.resolution.events({
+    'click .delete': function(){
+      Resolutions.remove(this._id);
+    }
+    
+   
+  });
 }
 
 if (Meteor.isServer) {
